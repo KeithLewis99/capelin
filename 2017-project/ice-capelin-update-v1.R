567f9d79dac39fc61a23ae6f1ddd28bb953d6351
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2017-10-24 16:59:03 -0230

    Added helper functions

--- 2017-project/ice-capelin-update-v1.R
+++ 2017-project/ice-capelin-update-v1.R
@@ -26,16 +26,23 @@ source("D:/Keith/capelin/2017-project/ice-capelin-functions.R")
 capelin <- read.csv('capelin-ice-2014.csv',header=T)
 # get rid of extra columns in csv file
 capelin <- capelin[c("year", "maxarea", "minlat", "tice", "capelin", "capelinlb", "capelinub", "logcapelin", "logcapelinlb", "logcapelinub")]
+capelin_ale <- capelin
+# from "acoustic_estimates_2017.xlsx" in capelin folder
+# add capelin, years, and lncapelin
+capelin[c(47, 48, 49), 5] <- matrix(c(662, NA, 158), ncol = 1) 
+capelin[c(47, 48, 49), 1] <- matrix(c(2015, 2016, 2017), ncol = 1) 
+capelin[c(47, 48, 49), 8] <- matrix(c(log(662), NA, log(158)), ncol = 1)
+
 # data set for joining to m1-m6 datasets
 capelin_join <- capelin[c("year", "capelin", "capelinlb", "capelinub", "logcapelin", "logcapelinlb", "logcapelinub")]
+
 
 # look at files in "output-processing" and create a file pattern
 data_path <- "output-processing"
 files <- list.files(data_path)
 pattern <- grep("capelin", files, value = T)
 # read the datasets and join them to capelin_join
-cape <- loadSubsetDatasets(capelin_join, pattern, 6)
-
+cape <- loadSubsetDatasets1(df = capelin_join, name = "capelin_m", pat = pattern, 6)
 
 ###############################################################################
 ## common features to all plots------
@@ -52,18 +59,22 @@ labtice <- expression(paste(italic(t[ice]), '(day of year)')) # label for figure
 ## Ale's original-------------
 ## Optimization - produces lists of the optimization curve for use in figures below
 # up to present
+source("D:/Keith/capelin/2017-project/ice-capelin-functions.R")
+capelin_ale$myear <- capelin_ale$year
+capelin_ale$myear[45:46] <- c(1960,1961)
 
-capelin$myear <- capelin$year
-capelin$myear[45:46] <- c(1960,1961)
+optim_Ale <- calcFit(capelin_ale)
 
-optim_Ale <- calcFit(capelin)
-
-optimGraphs(optim_Ale, regime1, regime2, yearInt, lnbiomassInt, "max-values:Ale")
+optimGraphs(optim_Ale$df, optim_Ale$regime1, optim_Ale$regime2, yearInt, lnbiomassInt, "max-values:Ale")
 ggsave("figs/u1-maxAle.pdf")
 
 ## m1--------------
+yearInt <- seq(1982, 2018, by=4)
+
 optim_m1 <- calcFit(cape$capelin_m1)
 
+#windows()
+#note that multiple windows can be opened but cowplot does not seem to recognize the news windoes and plots to the old one.  if you want to see plots in multiple widows, use multiplot but cannot label
 optimGraphs(optim_m1$df, optim_m1$regime1, optim_m1$regime2, yearInt, lnbiomassInt,  "max-values:m1")
 
 ggsave("figs/u2-maxm1.pdf")
@@ -99,21 +110,43 @@ ggsave("figs/u6-maxm5.pdf")
 
 ## m6--------------
 optim_m6 <- calcFit(cape$capelin_m6)
-str(optim_m6)
+windows()
 optimGraphs(optim_m6$df, optim_m6$regime1, optim_m6$regime2, yearInt, lnbiomassInt,  "max-values:m6")
 
 ggsave("figs/u7-maxm6.pdf")
 
-optim_m6$cdf
-
-# AIC for models
+# compare Sums of Squares
 2*optim_Ale$cdf$value+2*length(optim_Ale$cdf$par)
-2*optim_m1$cdf$value+2*length(optim_m1$cdf$par)
-2*optim_m2$cdf$value+2*length(optim_m2$cdf$par)
-2*optim_m3$cdf$value+2*length(optim_m3$cdf$par)
-2*optim_m4$cdf$value+2*length(optim_m4$cdf$par)
-2*optim_m5$cdf$value+2*length(optim_m5$cdf$par)
-2*optim_m6$cdf$value+2*length(optim_m6$cdf$par)
+
+
+optim_m1$cdf$value
+optim_m2$cdf$value
+optim_m3$cdf$value
+optim_m4$cdf$value
+optim_m5$cdf$value
+optim_m6$cdf$value
+
+
+
+
+# look at files in "output-processing" and create a file pattern
+data_path <- "output-processing"
+files <- list.files(data_path)
+pattern <- grep("sub1991-m+[^rsq].csv", files, value = T)
+# read the datasets and join them to capelin_join
+
+med_cape <- loadSubsetDatasets1(df = capelin_join, name = "med_m", pat = pattern, N = 6, var1 = "darea", var2 = "dminlats", nvar1 = "med_area", nvar2 = "minlats")
+
+ggplot(data = cape$capelin_m1, aes(x = year, y = capelin)) +
+     geom_point()
+
+ggplot(data = med_cape$med_m1, aes(x = year, y = capelin)) +
+     geom_point()
+
+ggplot(data = d5med_cape$d5med_m1, aes(x = year, y = capelin)) +
+     geom_point()
+
+source("D:/Keith/capelin/2017-project/ice-capelin-functions.R")
 #####################################################################################################
 #graphics.off()
 #####################################################################################################
@@ -147,32 +180,38 @@ ggplot(capelin, aes(x = year, y = logdiff)) +
    
 #####################################################################################################   
    
+   a <- 3
+   b <- 5
+   g <- 2
    
-## Ale's original changed from tice to area-------------
+   test <- a*capelin$tice
+   (1-capelin$tice/b)
+
+   ## Ale's original changed from tice to area-------------
 ## Optimization - produces lists of the optimization curve for use in figures below
 # up to present
-   CapelinDomeFit_area <- optim(par = c(1,500,0.6),
-                           dataf = capelin[which(capelin$logcapelin!='NA'),
+   CapelinDomeFit_area <- optim(par = c(2,400000,0.6),
+                           dataf = cape$capelin_m1[which(cape$capelin_m1$logcapelin!='NA'),
                                            c('year','maxarea','logcapelin')],
                            fn = SSQCapelinDome, method=c("BFGS"))
    
    # before 2010
-   CapelinDomeFitOld_area <- optim(par=c(0.2,500,0.6),
-                              dataf = capelin[which(capelin$year < 2011 & 
-                                                      capelin$logcapelin!='NA'),
+   CapelinDomeFitOld_area <- optim(par=c(2,400000,0.6),
+                              dataf = cape$capelin_m1[which(cape$capelin_m1$year < 2011 & 
+                                                                 cape$capelin_m1$logcapelin!='NA'),
                                               c('year','maxarea','logcapelin')],
                               fn = SSQCapelinDome, 
-                              method=c("BFGS"))
+                              method=c("L-BFGS-B"), control=list(maxit=50000))
    
    ## Obtain Expected Log Capelin Biomass using parameters estimated in lines above
-   capelin$ExpectedLogBiomass <- CapelinDome(params = c(CapelinDomeFit_area$par), dataf = capelin[,c('year','maxarea')])
+   cape$capelin_m1$ExpectedLogBiomass <- CapelinDome(params = c(CapelinDomeFit_area$par), dataf = cape$capelin_m1[,c('year','maxarea')])
    
-   capelin$ExpectedLogBiomassOld <- CapelinDome(params = c(CapelinDomeFitOld_area$par), dataf = capelin[,c('year','maxarea')])
+   cape$capelin_m1$ExpectedLogBiomassOld <- CapelinDome(params = c(CapelinDomeFitOld_area$par), dataf = cape$capelin_m1[,c('year','maxarea')])
    
    
    
    # attach the optimization curves of capelin abundance to ice data
-   xarea <- expand.grid(year = c(1990,2000), area = c(0:500,573.515,587.768))
+   xarea <- expand.grid(year = c(1990,2000), area = seq(from = 50000, to = 400000, by = 500))
    xarea <- xarea[order(xarea$area),]
    xarea$ExpectedLogBiomassOld <- CapelinDome(params = c(CapelinDomeFitOld_area$par),dataf = xarea)
    xarea$ExpectedLogBiomass <- CapelinDome(params = c(CapelinDomeFit_area$par),dataf = xarea)
@@ -189,7 +228,7 @@ ggplot(capelin, aes(x = year, y = logdiff)) +
    yearInt <- seq(1982, 2018, by=4)
    lnbiomassInt <- seq(0, 10, by=2)
    biomassInt <- seq(0, 8500)
-   labtice <- expression(paste(italic(t[ice]), '(day of year)')) # label for figures
+   labtice <- expression(paste(italic(t[area]), '(day of year)')) # label for figures
    
    windows()
 ggplot() +
@@ -197,10 +236,10 @@ ggplot() +
      geom_line(data = regime2, aes(x = area, y = ExpectedLogBiomass), colour="red", linetype=1, size=1.25) +
      geom_line(data = regime1, aes(x = area, y = ExpectedLogBiomassOld), colour="blue", linetype=1, size=1.25) +
      geom_line(data = regime2, aes(x = area, y = ExpectedLogBiomassOld), colour="blue", linetype=1, size=1.25) +
-     geom_point(data = subset(capelin, year < 1991), aes(x = maxarea, y = logcapelin), shape=2, size=3) +
-     geom_point(data = subset(capelin, year > 1991), aes(x = maxarea, y = logcapelin), shape=15, size=3) + 
-     geom_errorbar(data = subset(capelin, year < 1991), aes(x = maxarea, ymin=logcapelinlb, ymax=logcapelinub), width = 0.3, colour = "black") +
-     geom_errorbar(data = subset(capelin, year > 1991), aes(x = maxarea, ymin=logcapelinlb, ymax=logcapelinub), width = 0.3, colour = "black") +
+     geom_point(data = subset(cape$capelin_m1, year < 1991), aes(x = maxarea, y = logcapelin), shape=2, size=3) +
+     geom_point(data = subset(cape$capelin_m1, year > 1991), aes(x = maxarea, y = logcapelin), shape=15, size=3) + 
+     geom_errorbar(data = subset(cape$capelin_m1, year < 1991), aes(x = maxarea, ymin=logcapelinlb, ymax=logcapelinub), width = 0.3, colour = "black") +
+     geom_errorbar(data = subset(cape$capelin_m1, year > 1991), aes(x = maxarea, ymin=logcapelinlb, ymax=logcapelinub), width = 0.3, colour = "black") +
      xlab("maxarea") +
      ylab("ln (Capelin biomass (ktons))") + 
      ylim(0,9) +
