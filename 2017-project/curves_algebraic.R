65927ebe8d4c51359c93a7cb38885ba541cdafd5
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2017-11-28 16:22:32 -0330

    Simple scripts to look at curves

---
+++ 2017-project/curves_algebraic.R
@@ -0,0 +1,30 @@
+x <- c(-100:100)
+alpha <- 0.5 # increaes alpha, decrease |y|, negative turns parabola upside down
+beta <- 50 # increase beta, decrease  |y|
+gamma <- 2 # decrease gamma, decrease y
+z <- rnorm(201, mean=50, sd=10)
+hist(z)
+
+y1 <- alpha*x
+y2 <- 1-x/beta
+y3 <- alpha*x*(1-x/beta)
+y4 <- alpha*x*(1-x/beta)
+y5 <- y4 + gamma*z
+
+df <- cbind(y5, y4, x, z)
+head(df)
+
+plot(x, y1)
+plot(x, y2)
+plot(x, y3)
+plot(x, y4)
+plot(x, y5)
+lines(x, y5)
+
+
+alp <- MaxTice4$optim_ls$`MaxTice-m1`$cdf$par[1]
+bet <- MaxTice4$optim_ls$`MaxTice-m1`$cdf$par[2]
+
+x <- seq(0, 10, 0.1)
+y <- alp*x*(1-x/bet)
+plot(x, y)
