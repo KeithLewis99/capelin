0c3a3cabc178e6ec8973a01d0b99a0e769f0a631
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2017-12-08 11:31:09 -0330

    Store log back-transform code - removed from ice-capelin-covarites

--- 2017-project/curves_algebraic.R
+++ 2017-project/curves_algebraic.R
@@ -43,3 +43,78 @@ ylg <- log10(lx)
 ylg2 <- log2(lx)
 
 plot(lx, yln)
+
+
+
+# GOTO ice-capelin-covariates and run up to the MaxTice2 option - then, reproduce the below
+# this suggests to me that the normalizatoin is squishing the variable bc ratios are different
+x <- MaxTice2$optim_ls$`MaxTice-m1`$df
+rm(x)
+min(x$tice) - mean(x$tice)
+range(x$tice)
+max(x$tice)/min(x$tice)
+max(x$tice)
+min(x$Ntice) - mean(x$Ntice)
+range(x$Ntice)
+max(x$Ntice)/min(x$Ntice)
+max(x$Ntice)
+
+
+#attempt at back transforming
+# GOTO ice-capelin-covariates and run up to the MaxTice4 option - then, reproduce the below
+for(i in 1:length(MaxTice4$optim_ls)){
+     for(col in names(MaxTice4$optim_ls[[i]]$regime2)){
+          new_names <- c(1:3, NA)
+          new_names <- paste0("E", names(MaxTice4$optim_ls[[i]]$regime2[col]))
+          MaxTice4$optim_ls[[i]]$regime2[[new_names]] <- exp(MaxTice4$optim_ls[[i]]$regime2[[col]])
+     }
+}
+
+for(col in names(x)){
+     new_names <- c(1:3, NA)
+     new_names <- paste0("E", names(x[col]))
+     x[[new_names]] <- exp(x[[col]])
+}
+
+head(x)
+
+#won't work unless you add another var
+optimGraphs1_all(MaxTice4, "logtice", "optLog2")
+for(i in 1:length(MaxTice4$optim_ls)){
+     df1 <- as.data.frame(MaxTice4$optim_ls[[i]]$df)
+     df2 <- as.data.frame(MaxTice4$optim_ls[[i]]$regime1)
+     df3 <- as.data.frame(MaxTice4$optim_ls[[i]]$regime2)
+     mm <- optimGraphs1(df1, df2, df3, yearLim, yearInt, lnbiomassInt,  titlenames[i], "Elogtice", "tice")
+     ggsave(mm, filename = paste0("figs/covariates/optLog", titlenames[i], ".pdf"), width=10, height=8, units="in")
+}
+
+df1 <- as.data.frame(MaxTice4$optim_ls$`MaxTice-m1`$df)
+df2 <- as.data.frame(MaxTice4$optim_ls$`MaxTice-m2`$regime2)
+df3 <- as.data.frame(MaxTice4$optim_ls$`MaxTice-m1`$regime2)
+mm <- optimGraphs1(df1, df2, df3, yearLim, yearInt, lnbiomassInt,  "MaxTice-m1", "Elogtice", "tice")
+ggsave(mm, filename = paste0("figs/covariates/optLog", titlenames[i], ".pdf"), width=10, height=8, units="in")
+
+
+graphics.off()
+var1 <- "Elogtice"
+var2 <- "tice"
+p3 <- ggplot() + 
+     geom_line(data = df3, aes_string(x = var1, y = "ExpectedLogBiomass"), colour="red", linetype=1, size=1.25) + 
+     #geom_line(data = reg2, aes_string(x = var, y = "ExpectedLogBiomassOld"), colour="blue", linetype=1, size=1.25) +
+     geom_point(data = subset(df1, year > 1991), aes_string(x = var2, y = "logcapelin"), shape=15, size=3) + 
+     geom_errorbar(data = subset(df1, year > 1991), aes_string(x = var2, ymin="logcapelinlb", ymax="logcapelinub"), width = 0.3, colour = "black") +
+     xlab(paste(var2)) +
+     ylab("ln (Capelin biomass (ktons))") + 
+     #ylim(0,9) +
+     theme_bw()
+source("D:/Keith/capelin/2017-project/ice-capelin-covariates-FUN.R")
+
+# from Bolker
+x = 1:20
+a = 2
+b = 1
+
+y_det = a + b *x
+y = rnorm(20, mean = y_det, sd = 2)
+plot(x, y)
+lines(x, y)
