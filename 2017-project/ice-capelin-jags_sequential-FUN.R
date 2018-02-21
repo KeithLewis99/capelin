786f02f925777a1fd0f3c8a96e12f0ed51ae1ab4
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2018-02-21 17:03:07 -0330

    Added a function for priors, posteriors, and credible intervals

--- 2017-project/ice-capelin-jags_sequential-FUN.R
+++ 2017-project/ice-capelin-jags_sequential-FUN.R
@@ -16,7 +16,6 @@
 #' @examples plotCredInt(df2, yaxis = "ln_biomass_med", ylab = "ln(capelin)", y_line = y_med, ci_df2, pi_df2, model = txt, x = 2006, y = 8)
 
 plotCredInt <- function(df, yaxis = yaxis, ylab = ylab, y_line = y_line, ci = ci, dpi = dpi, model = model, x = x, y = y){
-     
      p <- ggplot()
      #browser()
      p <- p + geom_ribbon(aes(x = c(df$year, 2018:2019), 
@@ -26,8 +25,7 @@ plotCredInt <- function(df, yaxis = yaxis, ylab = ylab, y_line = y_line, ci = ci
      pi_n <- dpi[, (ncol(dpi)-1):ncol(dpi)]
      p <- p + geom_ribbon(aes(x = c(2018:2019), 
                               ymax = pi_n[2, ], 
-                              ymin = pi_n[1, ]),
-                          alpha = 0.5)
+                              ymin = pi_n[1, ]))
      p <- p + geom_text() +
           annotate("text", label = model, x = x, y = y, size = 7.5)
      p <- p + geom_point(data = df, 
@@ -37,10 +35,7 @@ plotCredInt <- function(df, yaxis = yaxis, ylab = ylab, y_line = y_line, ci = ci
      p <- p + xlab("Year") + ylab(paste(ylab))
      p <- p + theme(text = element_text(size=15)) + theme_bw()
      p <- p + geom_line(aes(x = c(df$year, 2018:2019), y = y_line))
-     
-     
      return(p)
-     
 }
 
 
@@ -63,6 +58,41 @@ priorPosterior <- function(df1, df2, xlim){
 
 
 
+
+
+#' Title
+#'
+#' @param df 
+#' @param df2 
+#' @param df3 
+#' @param limits 
+#' @param x_label 
+#' @param priormean 
+#' @param priorsd 
+#' @param by_bin 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+postPriors <- function(df, df2, df3, limits = limits, x_label = x_label, priormean, priorsd, by_bin = 1){
+     #browser()
+     p <- ggplot() + theme_bw()
+     p <- p + coord_cartesian(xlim = c(limits[1], limits[2])) 
+     p <- p + geom_histogram(data = as.data.frame(df), 
+                             aes(x = V1, y=..density..), 
+                             col="grey", fill="grey",
+                             #alpha=.6,
+                             breaks = seq(limits[1], limits[2], by=by_bin)) + xlab(x_label) + ylab("Density")
+     p <- p + geom_density(data = as.data.frame(df2), aes(x=df2), size = 1, alpha = 0.3) 
+     p <- p + geom_rug(data=data.frame(y=df3), aes(x=y), colour = "red", size=3, alpha=1) 
+     p <- p + geom_vline(aes(xintercept = 0), colour = "red", size = 2)
+     return(p)
+}
+
+
+
+
 # not using - not appropriate
 DIC_out <- function(df){
      browser()
@@ -72,30 +102,4 @@ DIC_out <- function(df){
      
 }
 
-
-# something not quite right with Ale's original code
-run_mortality_phigh$BUGSoutput$sims.list$beta[,3]
-run_mortality_phigh$sims.list$beta[,3]
-b3 <- run_mortality_phigh$BUGSoutput$sims.list$beta
-str(b3)
-
-b3 <- out.bin$sims.list$beta[,3]
-
-
-priormean <- 0
-priorsd <- 3
-prior <- rnorm(n = 1000, priormean, priorsd)
-plot(density(prior))
-limits <- c(min(b3)-0.3, max(b3) + 0.3)
-
-p <- ggplot(data = as.data.frame(prior), aes(x=prior)) + geom_density() 
-p <- p + coord_cartesian(xlim = c(limits[1], limits[2])) 
-p <- p + geom_histogram(data = as.data.frame(b3), 
-                        aes(x = b3, y=..density..), 
-                        col="white", fill="purple",
-                        alpha=.6,
-                        breaks = seq(limits[1], limits[2], by=0.01))
-p <- p + geom_rug(data=data.frame(y=b3), aes(x=y), size=2, alpha=1) 
-p <- p + geom_vline(aes(xintercept = 0), colour = "red") 
-# add data points to rug in white???
 
