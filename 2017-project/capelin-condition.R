1eba97bf729caedf128c23d704c36904f52b1806
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2018-04-09 16:17:48 -0230

    Major changes: annotation and automation

--- 2017-project/capelin-condition.R
+++ 2017-project/capelin-condition.R
@@ -6,7 +6,10 @@
 # 2) turn this into a markdown document for Fran
 # From "Fall acoustic and Campelen strat samples 1981-2015 from 2J3KL_v1.xlsx" from F. Mowbray
 # Uses "capelin_condition_maturation_v1.csv" - copied and pasted from above
-# generates "condition_out.csv - used in ice-capelin-jagps_sequential.R
+# generates "condition_out.csv - 
+# output used in ice-capelin-jagps_sequential.R and ice-caplein_jags_knock_out.R
+
+# there has been a lot of debate about this variable.  What age to use and what sex to use.  Age 1 capelin seem to make the most sense as that is the condition of capelin going into the second year.
 
 
 ## libraries------
@@ -14,6 +17,7 @@ library(ggplot2)
 library(readr)
 library(tidyr)
 library(dplyr)
+library(tidyselect)
 library(magrittr)
 
 rm(list=ls())
@@ -36,11 +40,15 @@ head(df_1617)
 df <- rbind(df, df_1617)
 glimpse(df)
 View(tail(df, 100))
+
+# save the combined data
 write_csv(df, "data/condition_1979_2017.csv")
+
+age <- 1
 
 #create one filter for ice-capelin project and one for Fran and the markdown doc
 df1 <- df %>%
-     filter(year > 1992 &  sex==1 & age == 2 & maturity != 6 & project != 10 & as.factor(month) %in% c("10", "11", "12") & as.factor(nafo_div) %in% c(23, 31, 32)) %>% #one-year males after 1992, just project 23 sex == 1 &
+     filter(year > 1992 &  sex==1 & age == 1 & maturity != 6 & project != 10 & as.factor(month) %in% c("10", "11", "12") & as.factor(nafo_div) %in% c(23, 31, 32)) %>% #one-year males after 1992, just project 23 sex == 1 &
      filter(!is.na(weight)) %>%
      filter(!is.na(length)) 
 
@@ -56,7 +64,7 @@ levels(df1$nafo_div)[levels(df1$nafo_div) == "32"] <- "3L"
 levels(df1$sex)[levels(df1$sex) == "1"] <- "Male"
 levels(df1$sex)[levels(df1$sex) == "2"] <- "Female"
 
-# look at levels of data
+# check levels of data to ensure that subset worked
 levels(df1$project)
 levels(as.factor(df1$sample_number))
 levels(as.factor(df1$year))
@@ -67,27 +75,34 @@ range(df1$weight)
 range(df1$length)
 levels(as.factor(df1$age))
 
-# filter out 99% by year - how to do this????
+# sample size by year
+count_n <- df1 %>%
+     group_by(year) %>%
+     summarize(n = n())
+
+
+# remove outliers: filter out 99% by year - how to do this????
+# not sure why I did what I did here
 summary(df1$weight)
 mean_w <- mean(df1$weight)
 sd_w <- sd(df1$weight)
 nrow(df1)
 uci <- mean_w + qnorm(0.995)*sd_w/sqrt(nrow(df1))
-quantile(df1$weight, c(0.5, 0.95, 0.99, 0.995, 0.999))
 
-count_n <- df1 %>%
-     group_by(year) %>%
-     summarize(n = n())
+quantile(df1$weight, c(0.01, 0.1, 0.5, 0.95, 0.99, 0.995, 0.999))
+quantile(df1$length, c(0.01, 0.1, 0.5, 0.95, 0.99, 0.995, 0.999))
 
 quant <- df1 %>%
      group_by(year) %>%
-     summarize(uq = quantile(weight, c(0.99)), lq = quantile(length, c(0.01)))
+     summarize(wuq = quantile(weight, c(0.99)), wlq = quantile(weight, c(0.01)), luq = quantile(length, c(0.99)), llq = quantile(length, c(0.01)))
 
-# for age 2
+
+# filter out heavy and very short
 df1 <- df1 %>%
      group_by(year) %>%
      right_join(x=df1, y=quant, by ="year") %>%
-     filter(weight < uq & length > lq)
+     filter(weight < wuq & weight > wlq) %>%
+     filter(length < luq & length > llq)
 
 
 ## DATA EXPLORATION - ZUUR 2010----
@@ -123,7 +138,8 @@ ggplot(data=df1) +
      geom_boxplot(aes(x = as.factor(month), y = weight))
 
 ggplot(data=df1) +
-     geom_boxplot(aes(x = as.factor(month), y = weight)) +      facet_wrap(~nafo_div)
+     geom_boxplot(aes(x = as.factor(month), y = weight)) +      
+     facet_wrap(~nafo_div)
 
 
 # nafo_div (3L lower length and weight)- probably older bc of latter in season
@@ -193,29 +209,56 @@ par(mfrow = c(1,1))
 # 3 outliers; homogeneity looks good other than than.  Normality stinks but probably not a problem - very few values.
 # 2405, 3962, 2227 - outliers before filtering for 99%
 
-# new outliers
-data.frame(df1[2006, c("month", "length", "weight")])# short and heavy
-data.frame(df1[1828, c("month", "length", "weight")]) # long and light
-data.frame(df1[3542, c("month", "length", "weight")])# long and light
-#leverage
-data.frame(df1[332, c("month", "length", "weight")])# short and light
+if(age == 2){
+     # new outliers
+     data.frame(df1[2006, c("month", "length", "weight")])# short and heavy
+     data.frame(df1[1828, c("month", "length", "weight")]) # long and light
+     data.frame(df1[3542, c("month", "length", "weight")])# long and light
+     #leverage
+     data.frame(df1[332, c("month", "length", "weight")])# short and light
+     df1 <- df1[-c(2006, 1828, 3542, 332), ]
+} else if (age == 1){
+     # check and remove outliers for age 1 - rerun
+     df1[1954, c("month", "length", "weight")] # very light
+     df1[3036, c("month", "length", "weight")] # very heavy
+     df1[588, c("month", "length", "weight")] # l/m
+     #leverage
+     data.frame(df1[324, c("month", "length", "weight")])# very, very short and light
+     data.frame(df1[4292, c("month", "length", "weight")])# 
+     data.frame(df1[1954, c("month", "length", "weight")])# 
+     str(df1)
+     df1 <- df1[-c(1801, 1978, 3491, 328), ]
+}
 
-# check and remove outliers for age 2 - rerun
-data.frame(df1[1343, c("month", "length", "weight")])# l/h
-data.frame(df1[173, c("month", "length", "weight")]) # s/l
-data.frame(df1[2363, c("month", "length", "weight")])# l/l
-df1 <- df1[-c(1343, 173, 2363), ]
+#######STOP - re-run m1 and assess for new outliers
+if(age == 2){
+     # check and remove outliers for age 2 - rerun
+     data.frame(df1[1343, c("month", "length", "weight")])# l/h
+     data.frame(df1[173, c("month", "length", "weight")]) # s/l
+     data.frame(df1[2363, c("month", "length", "weight")])# l/l
+     df1 <- df1[-c(1343, 173, 2363), ]
+} else if (age == 1){
+     data.frame(df1[1952, c("month", "length", "weight")])# l/h
+     data.frame(df1[587, c("month", "length", "weight")])# l/h
+     data.frame(df1[3033, c("month", "length", "weight")])# l/h
+     #leverage
+     data.frame(df1[1952, c("month", "length", "weight")])
+     data.frame(df1[4288, c("month", "length", "weight")])
+     data.frame(df1[324, c("month", "length", "weight")])
+     df1 <- df1[-c(1952, 587, 3033, 1952, 4288, 324), ]
+}
 
-# check and remove outliers for age 1 - rerun
-df1[1978, c("month", "length", "weight")] # s/h
-df1[3491, c("month", "length", "weight")] # l/l
-df1[1801, c("month", "length", "weight")] # l/m
-str(df1)
-df1 <- df1[-c(1801, 1978, 3491), ]
-
-
+     data.frame(df1[3065, c("month", "length", "weight")])
+     data.frame(df1[3055, c("month", "length", "weight")])
+     data.frame(df1[1837, c("month", "length", "weight")])
+     #leverage
+     data.frame(df1[2921, c("month", "length", "weight")])
+     df1 <- df1[-c(194, 4787, 2858), ]
+     df1 <- df1[-c(3018, 2888, 257, 1812), ]
+     
 ##output----
-df1$fits <- fitted(m1)
+##### STOP - rerun m1
+df1$fits <- fitted(m1) 
 m1$fitted.values
 str(m1)
 df1$rel.cond <- df1$weight/10^df1$fits
@@ -292,20 +335,25 @@ ggplot(data=df2) +
      facet_wrap(~year)
 
 
-# produce output for Bayesain analysis
+# produce output for Bayesain analysis 1999-2017
 out <- df2 %>%
      group_by(year) %>%
      #filter(nafo_div == "3K" & month == 11) %>%
      summarize(meanCond = round(mean(rel.cond),4), stdCond= round(sd(rel.cond),4), medCond = round(median(rel.cond), 4))
 View(out)
-write_csv(out, "data/condition_ag1_out.csv")
-#this was for data to 2015 only
+
+if(age == 1){
+     write_csv(out, "data/condition_ag1_out.csv")     
+} else if (age == 2){
+     write_csv(out, "data/condition_ag2a_out.csv")
+}
+
+#note that the below file was for 2015 only
 #write_csv(out, "data/condition_ag2_out.csv")
-write_csv(out, "data/condition_ag2a_out.csv")
 
 
 ##Residuals----
-## compare observed/expected to residuals - is there a difference
+## compare observed/expected to residuals - is there a difference?
 condResids <- read.csv('data/archive/condition_resids.csv')
 str(condResids)
 condResids[19:20,2] <- NA
@@ -316,6 +364,7 @@ View(condResids)
 condResids$resids_lag <- lag(condResids$resids, 1)
 
 # compare cond/exp and resids
+# NOTE that these are NOT very comparable
 comp <- left_join(out, condResids, by = "year")
 comp <- subset(comp, year > 1998)
 plot(comp$meanCond, comp$resids) 
@@ -324,6 +373,7 @@ points(comp$meanCond[6], comp$resids[6], col = "red") #show 2004
 
 
 # can we reproduce Fran's results
+# the answer is NO (I think)
 glimpse(df)
 glimpse(df1)
 df10 <- df %>%
@@ -339,7 +389,6 @@ df10 <- df %>%
 m1 <- lm(log10(length) ~ log10(weight), data = df10)
 fits <- m1$fitted.values
 res <- m1$residuals
-names()
 df11 <- cbind(df10, fits, res)
 
 head(df11)
@@ -349,54 +398,83 @@ temp <- df11 %>%
 df1
 levels(as.factor(df1$nafo_div))
 
+
 ##Age1 M/F comparisons----
-df1 <- df %>%
-     filter(year > 1992 &  age == 2 & maturity != 6 & project != 10 & as.factor(month) %in% c("10", "11", "12") & as.factor(nafo_div) %in% c(23, 31, 32)) %>% #one-year males after 1992, just project 23 sex == 1 &
+# there is considerable worry that using the males only will cause questions during the review process.  The intent of the following analysis is to test for differences.
+
+df2 <- df %>%
+     filter(year > 1992 &  age == 1 & maturity != 6 & project != 10 & as.factor(month) %in% c("10", "11", "12") & as.factor(nafo_div) %in% c(23, 31, 32)) %>% #one-year males after 1992, just project 23 sex == 1 &
      filter(!is.na(weight)) %>%
      filter(!is.na(length)) 
 
 cols <- c("project", "nafo_div", "sex", "maturity")
-df1 %<>%
+df2 %<>%
      mutate_each_(funs(factor(.)),cols)
-glimpse(df1)
+glimpse(df2)
 
-df1$nafo_div <- as.factor(df1$nafo_div)
-levels(df1$nafo_div)[levels(df1$nafo_div) == "23"] <- "2J"
-levels(df1$nafo_div)[levels(df1$nafo_div) == "31"] <- "3K"
-levels(df1$nafo_div)[levels(df1$nafo_div) == "32"] <- "3L"
-levels(df1$sex)[levels(df1$sex) == "1"] <- "Male"
-levels(df1$sex)[levels(df1$sex) == "2"] <- "Female"
+df2$nafo_div <- as.factor(df2$nafo_div)
+levels(df2$nafo_div)[levels(df2$nafo_div) == "23"] <- "2J"
+levels(df2$nafo_div)[levels(df2$nafo_div) == "31"] <- "3K"
+levels(df2$nafo_div)[levels(df2$nafo_div) == "32"] <- "3L"
+levels(df2$sex)[levels(df2$sex) == "1"] <- "Male"
+levels(df2$sex)[levels(df2$sex) == "2"] <- "Female"
 
-glimpse(df1)
-
-df1.m <- filter(df1, sex == "Male")
-m2.m <- lm(log10(weight) ~ log10(length), data= df1.m)
-
-df1.f <- filter(df1, sex == "Female")
-m2.f <- lm(log10(weight) ~ log10(length), data= df1.f)
-
-
-df1.m$fits <- fitted(m2.m)
-df1.f$fits <- fitted(m2.f)
-glimpse(df1.f)
-
-m2$fitted.values
-str(m2)
-df1.m$rel.cond <- df1.m$weight/10^df1.m$fits
-
-df1.m$resids <- df1.m$weight-10^df1.m$fits
-
-plot(df1.m$rel.cond, df1.m$resids)
-
-df1.f$rel.cond <- df1.f$weight/10^df1.f$fits
-df1.f$resids <- df1.f$weight-10^df1.f$fits
-
-plot(df1.f$rel.cond, df1.f$resids)
+# check levels of data to ensure that subset worked
+levels(df2$project)
+levels(as.factor(df2$sample_number))
+levels(as.factor(df2$year))
+levels(as.factor(df2$month))
+levels(df2$nafo_div)
+levels(df2$maturity)
+range(df2$weight)
+range(df2$length)
+levels(as.factor(df2$age))
 
 
-head(df1.f)
+quant <- df2 %>%
+     group_by(year) %>%
+     summarize(wuq = quantile(weight, c(0.99)), wlq = quantile(weight, c(0.01)), luq = quantile(length, c(0.99)), llq = quantile(length, c(0.01)))
 
-temp <- rbind(df1.m, df1.f)
+
+# filter out heavy and very short
+df2 <- df2 %>%
+     group_by(year) %>%
+     right_join(x=df2, y=quant, by ="year") %>%
+     filter(weight < wuq & weight > wlq) %>%
+     filter(length < luq & length > llq)
+
+# run models for male and females seperately
+df2.m <- filter(df2, sex == "Male")
+m2.m <- lm(log10(weight) ~ log10(length), data= df2.m)
+
+df2.f <- filter(df2, sex == "Female")
+m2.f <- lm(log10(weight) ~ log10(length), data= df2.f)
+
+
+df2.m$fits <- fitted(m2.m)
+df2.f$fits <- fitted(m2.f)
+glimpse(df2.f)
+
+m2.m$fitted.values
+str(m2.m)
+df2.m$rel.cond <- df2.m$weight/10^df2.m$fits
+
+df2.m$resids <- df2.m$weight-10^df2.m$fits
+
+
+df2.m <- df2.m %>%
+     filter(rel.cond <1.3 & rel.cond > 0.75)
+
+plot(df2.m$rel.cond, df2.m$resids)
+
+df2.f$rel.cond <- df2.f$weight/10^df2.f$fits
+df2.f$resids <- df2.f$weight-10^df2.f$fits
+df2.f <- df2.f %>%
+     filter(rel.cond <1.3 & rel.cond > 0.75)
+
+plot(df2.f$rel.cond, df2.f$resids)
+
+temp <- rbind(df2.m, df2.f)
 
 # produce table
 
@@ -425,9 +503,7 @@ ggplot(data=df2) +
      facet_wrap(~sex, ncol=1) + 
      geom_hline(aes(yintercept = 1), colour = 'red') +
      theme_bw()
-levels(as.factor(df1$year))
-
-
+levels(as.factor(df2$year))
 
 # there appear to be a few outliers here but they should have minimal influence on the final outcome
 
