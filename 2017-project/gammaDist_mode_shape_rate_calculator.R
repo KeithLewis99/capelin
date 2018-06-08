961504c29036c1d0b2e115a773805f8517fc79aa
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2018-06-08 16:41:21 -0230

    Modified code based on discussion with Ale; see Gamma distribution notes

--- 2017-project/gammaDist_mode_shape_rate_calculator.R
+++ 2017-project/gammaDist_mode_shape_rate_calculator.R
@@ -1,8 +1,8 @@
 # http://doingbayesiandataanalysis.blogspot.ca/2012/01/parameterizing-gamma-distribution-by.html
 
 # Specify desired mode and sd of gamma distribution:
-mode = 1.86
-sd = 1
+mode = 1.86 # this is Ale's value from 2014 paper for Beta (width of dome)
+sd = .6 # this is choosen arbitrarily but signifies two months (60days/100) for the width which means a month for the peak (Beta/2) which allows for reasonable values of the peak.
 
 # Here are the corresponding rate and shape parameter values:
 ra = ( mode + sqrt( mode^2 + 4*sd^2 ) ) / ( 2 * sd^2 )
@@ -10,6 +10,7 @@ sh = 1 + mode * ra
 
 show(sh)
 show(ra)
+show(1/ra)
 
 # Graph it:
 x = seq(0,mode+5*sd,len=1001)
@@ -19,7 +20,17 @@ plot( x , dgamma( x , shape=sh , rate=ra ) , type="l" ,
                  sep="") )
 abline( v=mode , lty="dotted" )
 
+plot( x , dgamma( x , shape=sh , rate=ra ) , type="l" , 
+      main=paste("dgamma, mode=", round(mode, 2),", \n, shape = " , round(sh, 2),", rate=", round(ra, 2), sep="") ,
+      ylab=paste("dgamma( shape=",signif(sh,3)," , rate=",signif(ra,3)," )",
+                 sep="") )
+abline( v=mode , lty="dotted" )
 
+plot( x , dgamma( x , shape=2.8 , rate=1 ) , type="l" , 
+      main=paste("dgamma, mode=", round(mode, 2),", \n, shape = " , round(sh, 2),", rate=", round(ra, 2), sep="") ,
+      ylab=paste("dgamma( shape=",signif(sh,3)," , rate=",signif(ra,3)," )",
+                 sep="") )
+abline( v=mode , lty="dotted" )
 
 x <- seq(1, 100, 1)
 y <- log(x)
@@ -29,3 +40,14 @@ plot(x, y)
 x <- seq(100, 5000, 10)
 y <- log(x)
 plot(x, y)
+
+
+estimate_mode <- function(x) {
+     d <- density(x)
+     d$x[which.max(d$y)]
+}
+
+
+mode = estimate_mode(ice1$tice*2/100)
+mean(ice1$tice*2/100)
+sd = sd(ice1$tice*2/100)
